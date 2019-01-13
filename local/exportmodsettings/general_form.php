5ae63783073af0240136e9605332cac2647f90b5
oleg <oleg@devlion.co>
2019-01-13 15:11:18 +0200

    Add local/exportmodsettings

---
+++ local/exportmodsettings/general_form.php
@@ -0,0 +1,130 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../../config.php');
+require_once($CFG->dirroot . '/lib/formslib.php');
+require_once('locallib.php');
+
+class general_form extends moodleform {
+
+    public $ifdownload = false;
+    public $postdata;
+
+	public function definition() {
+
+		$mform = $this->_form; // Don't forget the underscore!
+		//$mform->addElement('hidden', 'id', $this->_customdata['id']);
+		//$mform->setType('id', PARAM_RAW);
+
+        //Cron time
+        $mform->addElement('header', 'general', get_string('cron_settings', 'local_exportmodsettings'));
+        $mform->setExpanded('general', true);
+
+        $attributes = array();
+        $select = $mform->addElement('select', 'crontime', get_string('crontime', 'local_exportmodsettings'), SETTINGSCRONPERIODSSELECT, $attributes);
+        $select->setMultiple(false);
+
+        $mform->addElement('submit', 'submitcrontime', get_string('savechanges'));
+
+        //export mod settings
+        $mform->addElement('header', 'export', get_string('export_mod', 'local_exportmodsettings'));
+        $mform->setExpanded('export', true);
+
+		//Year
+        $attributes = array();
+        $selectyears = array();
+        $currentyear = date("Y");
+
+        $selectyears[0] =  get_string('all');
+        for($i = $currentyear-9; $i < $currentyear+1; $i++){
+            $selectyears[$i] = $i;
+        }
+
+        $select = $mform->addElement('select', 'year', get_string('year', 'local_exportmodsettings'), $selectyears, $attributes);
+        $select->setMultiple(false);
+
+        //Semester
+        $attributes = array();
+        $select = $mform->addElement('select', 'semester', get_string('semester', 'local_exportmodsettings'), SETTINGSTYPESEMESTERVIEW, $attributes);
+        $select->setMultiple(false);
+
+        //Date
+        $mform->addElement('date_selector', 'startdate', get_string('start_date', 'local_exportmodsettings'));
+        $mform->addElement('date_selector', 'enddate', get_string('end_date', 'local_exportmodsettings'));
+
+        //Set default date
+        $defaulttime = time() - 30*24*60*60;
+        $mform->setDefault('startdate',  $defaulttime);
+
+        $mform->addElement('submit', 'exportfile', get_string('export_file', 'local_exportmodsettings'));
+
+	}
+
+    function validation($data, $files) {
+        $errors = parent::validation($data, $files);
+
+        //Export excel
+        if(isset($data['exportfile'])){
+            if($data['startdate'] > $data['enddate']){
+                $errors['enddate'] = get_string('wrong_dates', 'local_exportmodsettings');;
+            }
+        }
+
+        return $errors;
+    }
+
+    function set_default_data() {
+        global $USER, $CFG, $DB;
+
+        $defaultdata = array();
+
+        //crontime
+        $row = $DB->get_record('config_plugins', array('plugin' => 'local_exportmodsettings', 'name' => 'crontime'));
+        if(!empty($row)){
+            $defaultdata['crontime'] = $row->value;
+        }
+
+        //$defaultdata['year'] = date("Y");
+        $defaultdata['year'] = 0;
+
+        parent::set_data($defaultdata);
+    }
+
+    function get_data() {
+        global $DB, $USER, $CFG;
+
+        $data = parent::get_data();
+        $this->postdata = $data;
+
+        //Save crontime
+        if(isset($data->submitcrontime)){
+            $row = $DB->get_record('config_plugins', array('plugin' => 'local_exportmodsettings', 'name' => 'crontime'));
+            if(!empty($row)){
+                $row->value = $data->crontime;
+                $DB->update_record('config_plugins', $row);
+            }else{
+                $obj = new \stdClass();
+                $obj->plugin = 'local_exportmodsettings';
+                $obj->name = 'crontime';
+                $obj->value = $data->crontime;
+                $DB->insert_record('config_plugins', $obj);
+            }
+        }
+
+        //Export excel
+        if(isset($data->exportfile)){
+            $this->ifdownload = true;
+        }
+
+        //echo '<pre>';print_r($data);exit;
+    }
+
+    public function is_download() {
+	    return $this->ifdownload;
+    }
+
+    public function download() {
+        if($this->ifdownload){
+            local_exportmodsettings_download_file($this->postdata);
+        }
+    }
+}
---
+++ local/exportmodsettings/locallib.php
@@ -0,0 +1,322 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package local_exportmodsettings
+ * @author Mike Churchward <mike.churchward@poetgroup.org>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright 2016 POET
+ */
+
+
+/**
+ * Hook function to extend the course settings navigation. Call all context functions
+ */
+
+define("SETTINGSCRONPERIODSSELECT", array(
+    0 => get_string('day'),
+    1 => get_string('twodays', 'local_exportmodsettings'),
+    2 => get_string('week'),
+    3 => get_string('month'),
+    4 => get_string('all')
+));
+
+define("SETTINGSCRONPERIODS", array(
+    0 => 24*60*60, //sec
+    1 => 2*24*60*60, //sec
+    2 => 7*24*60*60, //sec
+    3 => 30*24*60*60, //sec
+    4 => 0 //all
+));
+
+define("SETTINGSTYPESEMESTER", array(
+    'A' => '001',
+    'B' => '002',
+    'C' => '003',
+));
+
+define("SETTINGSTYPESEMESTERVIEW", array(
+    '0' => get_string('all'),
+    'A' => get_string('char_a', 'local_exportmodsettings'),
+    'B' => get_string('char_b', 'local_exportmodsettings'),
+    'C' => get_string('char_c', 'local_exportmodsettings'),
+));
+
+define("SETTINGSTYPEASSIGN", array(
+    '1' => get_string('type_assign_1', 'local_exportmodsettings'),
+    '11' => get_string('type_assign_2', 'local_exportmodsettings'),
+    '12' => get_string('type_assign_3', 'local_exportmodsettings'),
+));
+
+define("SETTINGSCATEGORYOFFSET", 90000);
+
+function local_exportmodsettings_generate_output_csv($output, $postdata = array()){
+    global $DB;
+
+    $num = 0;
+    $data = array();
+    $usedids = array();
+
+    $headers = array(
+        'YEAR',
+        'SEMESTER',
+        'SM_OBJID',
+        'E_OBJID',
+        'MOODLE_ID',
+        'ASSIGN_NAME',
+        'WEIGHT',
+        'OBLIGATORY',
+        'PASS_GRADE',
+        'ASSIGN_REQ',
+        'ASSIGN_FOR_AVG',
+        'PARENT_ASSIGN',
+        'SUPPORTIVE_GRADE',
+        'ASSIGN_TYPE',
+        'LAST_UPDATED',
+    );
+
+    $listmods = array();
+
+    //Get list mods
+    $sql = "
+      SELECT DISTINCT itemmodule
+      FROM {grade_items}
+      WHERE itemmodule IS NOT NULL
+    ";
+
+    $result = $DB->get_records_sql($sql);
+
+    foreach($result as $item){
+        if($item->itemmodule != 'quiz'){
+            $listmods[] = $item->itemmodule;
+        }
+    }
+
+    //Start test time execute
+    $start = microtime(true);
+
+    foreach($listmods as $mod) {
+        $query = "
+        SELECT
+            gi.id,
+            c.shortname AS course_name,
+            c.idnumber AS course_idnumber,
+            
+            IF(gi.itemtype='category', gi.iteminstance+".SETTINGSCATEGORYOFFSET.", gi.iteminstance ) AS moodle_id,            
+            
+            IF(gi.itemtype='category', gc.fullname, a.name ) AS assign_name,
+            gi.aggregationcoef2*100 AS weight,
+            IF(gi.hidden = 0, 1, '' ) AS obligatory,
+            gi.gradepass AS pass_grade,
+            IF(gi.itemtype='category', 
+                (
+                    SELECT COUNT(*)
+                    FROM {grade_items} AS sgi
+                    WHERE sgi.categoryid=gc.id AND sgi.hidden=0
+                )
+            , '' ) AS assign_req,
+            IF(gi.itemtype='category', 
+                (
+                    SELECT COUNT(*)
+                    FROM {grade_items} AS sgi
+                    WHERE sgi.categoryid=gc.id AND sgi.hidden=0
+                )
+            , '' ) AS assign_for_avg,
+            IF(gi.itemmodule='".$mod."', gi.categoryid, '' ) AS parent_assign,
+            
+            REPLACE(IF(gi.itemtype!='category', gcd.path, '' ),'/','') AS assign_type, 
+            
+            GREATEST(a.timemodified, gi.timemodified) AS last_updated
+            
+        FROM {grade_items} AS gi
+        LEFT JOIN {course} AS c ON (c.id = gi.courseid)        
+        LEFT JOIN {grade_categories} AS gc ON (gc.id = gi.iteminstance)
+        LEFT JOIN {grade_categories} AS gcd ON (gcd.id = gi.categoryid)
+        LEFT JOIN {".$mod."} AS a ON (a.id = gi.iteminstance)         
+    ";
+
+        //If used in cron
+        if (empty($postdata)) {
+            $row = $DB->get_record('config_plugins', array('plugin' => 'local_exportmodsettings', 'name' => 'crontime'));
+            $periodago = SETTINGSCRONPERIODS[$row->value];
+
+            if ($periodago != 0) {
+                $attributes = array(time() - $periodago);
+                $select = " WHERE (gi.itemmodule='".$mod."' OR gi.itemtype='category') 
+                    AND GREATEST(a.timemodified, gi.timemodified) > ?  ";
+            } else {
+                $attributes = array();
+                $select = " WHERE (gi.itemmodule='".$mod."' OR gi.itemtype='category') ";
+            }
+        }
+
+        //If used in download file
+        if (!empty($postdata) and isset($postdata->exportfile)) {
+            $attributes = array($postdata->startdate, $postdata->enddate);
+
+            $select = " 
+                WHERE (gi.itemmodule='".$mod."' OR gi.itemtype='category') 
+                AND GREATEST(a.timemodified, gi.timemodified) BETWEEN ? AND ?                       
+            ";
+
+            if($postdata->year != 0){
+                $year = '-' . $postdata->year;
+                $select .= " AND c.shortname LIKE('%" . $year . "%') ";
+            }
+
+            if($postdata->semester != '0'){
+                $semester = '-' . $postdata->semester;
+                $select .= " AND c.shortname LIKE('%" . $semester . "%') ";
+            }
+
+        }
+
+        $query .= $select;
+
+        $result = $DB->get_records_sql($query, $attributes);
+
+        foreach ($result as $item) {
+
+            if(in_array($item->id, $usedids)) continue;
+
+            //Prepare YEAR and SEMESTER
+            $arrname = explode('-', $item->course_name);
+            $yearvalue = (isset($arrname[3])) ? $arrname[3] - 1 : '';
+            $semestrvalue = (isset($arrname[2])) ? SETTINGSTYPESEMESTER[preg_replace("/[^a-zA-Z]+/", "", $arrname[2])] : '';
+
+            //Prepare SM_OBJID and E_OBJID
+            $arridnumber = explode('-', $item->course_idnumber);
+            $smobjid = (isset($arridnumber[1])) ? $arridnumber[1] : '';
+            $eobjid = (isset($arridnumber[0])) ? $arridnumber[0] : '';
+
+            //Validation
+            if(empty($yearvalue) || strlen($yearvalue) != 4 || !is_numeric($yearvalue)) continue;
+            if(empty($semestrvalue)) continue;
+            if(empty($smobjid) || !is_numeric($smobjid)) continue;
+            if(empty($eobjid) || !is_numeric($eobjid)) continue;
+
+            $data[$num]['YEAR'] = $yearvalue;
+            $data[$num]['SEMESTER'] = $semestrvalue;
+            $data[$num]['SM_OBJID'] = $smobjid;
+            $data[$num]['E_OBJID'] = $eobjid;
+
+            $data[$num]['MOODLE_ID'] = $item->moodle_id;
+            $data[$num]['ASSIGN_NAME'] = $item->assign_name;
+            $data[$num]['WEIGHT'] = $item->weight;
+            $data[$num]['OBLIGATORY'] = $item->obligatory;
+            $data[$num]['PASS_GRADE'] = $item->pass_grade;
+
+            $data[$num]['ASSIGN_REQ'] = $item->assign_req;
+            $data[$num]['ASSIGN_FOR_AVG'] = $item->assign_for_avg;
+            $data[$num]['PARENT_ASSIGN'] = $item->parent_assign;
+            $data[$num]['SUPPORTIVE_GRADE'] = '';//???
+
+            $data[$num]['ASSIGN_TYPE'] = (!empty($item->assign_type))?SETTINGSTYPEASSIGN[$item->assign_type]:'';
+            $data[$num]['LAST_UPDATED'] = date('Ymd', $item->last_updated);
+
+            $num++;
+            $usedids[] = $item->id;
+        }
+    }
+
+    $time_elapsed_secs = microtime(true) - $start;
+    local_exportmodsettings_log_file_success('Process took  '.$time_elapsed_secs.' sec');
+    //End test time execute
+
+    //headers
+    fputcsv($output, $headers);
+    foreach($data as $row) {
+        fputcsv($output, $row);
+    }
+
+    return $output;
+}
+
+function local_exportmodsettings_save_file_to_disk(){
+    global $DB, $CFG;
+
+    local_exportmodsettings_log_file_success('Start cron');
+
+    $folderPath = $CFG->dataroot.'/sap';
+    $filename = 'MoodleAssg-'.date("Ymd").'.csv';
+    $pathToFile = $folderPath.'/'.$filename;
+
+    //Create folder if not exists
+    if(!file_exists($folderPath)){
+        if (!mkdir($folderPath, 0755, true)){
+            die("No permission for ".$folderPath);
+        }
+    }
+
+    //If file present
+    if(file_exists($pathToFile)){
+        local_exportmodsettings_log_file_error("File present ".$pathToFile);
+        //die("File present ".$pathToFile);
+    }
+
+    $output = fopen($pathToFile,'w') or die("Can't open ".$pathToFile);
+    $output = local_exportmodsettings_generate_output_csv($output);
+    fclose($output) or die("Can't close ".$pathToFile);
+
+    local_exportmodsettings_log_file_success('End cron. Saved to file '.$filename);
+}
+
+function local_exportmodsettings_download_file($postdata){
+    global $DB, $CFG;
+
+    local_exportmodsettings_log_file_success('Start download');
+
+    $filename = 'MoodleAssg-'.date("Ymd").'.csv';
+
+    header("Content-type: application/csv");
+    header("Content-Disposition: attachment; filename=".$filename);
+
+    $output = fopen('php://output', 'w');
+    $output = local_exportmodsettings_generate_output_csv($output, $postdata);
+    fclose($output);
+
+    local_exportmodsettings_log_file_success('End download');
+    exit;
+}
+
+function local_exportmodsettings_log_file($status, $str){
+    global $DB, $CFG;
+
+    $folderPath = $CFG->dataroot.'/sap';
+    $filename = 'log_process_settings.txt';
+    $pathToFile = $folderPath.'/'.$filename;
+
+    //Create folder if not exists
+    if(!file_exists($folderPath)){
+        if (!mkdir($folderPath, 0755, true)){
+            die("No permission for ".$folderPath);
+        }
+    }
+
+    $output = fopen($pathToFile,'a') or die("Can't open ".$pathToFile);
+
+    $data = $status.' '.date("Y-m-d H:i:s").' '.$str.PHP_EOL;
+    fwrite($output, $data);
+    fclose($output) or die("Can't close ".$pathToFile);
+}
+
+function local_exportmodsettings_log_file_success($str){
+    local_exportmodsettings_log_file('SUCCESS', $str);
+}
+
+function local_exportmodsettings_log_file_error($str){
+    local_exportmodsettings_log_file('ERROR', $str);
+}
