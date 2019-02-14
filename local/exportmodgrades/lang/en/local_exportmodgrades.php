6619d576537e99f231eede5cade2d88d56ef17dc
oleg.och@gmail.com <oleg.och@gmail.com>
2019-02-14 13:18:54 +0200

    Add local/exportmodgrades

---
+++ local/exportmodgrades/lang/en/local_exportmodgrades.php
@@ -0,0 +1,50 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     local_exportmodgrades
+ * @category    string
+ * @copyright   2017 nadavkav@gmail.com
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Export mod grades';
+$string['page_index'] = 'Settings and export mod grades';
+$string['export_mod'] = 'Export mod grades';
+$string['cron_grades'] = 'Cron grades';
+$string['export_file'] = 'Export file';
+$string['year'] = 'Year';
+$string['semester'] = 'Semester';
+$string['start_date'] = 'From change date';
+$string['end_date'] = 'Until date of change';
+$string['crontime'] = 'Automatic execution period';
+$string['twodays'] = 'two days';
+
+$string['type_assign_1'] = 'מטלת אב';
+$string['type_assign_2'] = 'מטלת בן';
+$string['type_assign_3'] = 'מטלה רגילה';
+
+$string['char_a'] = 'A';
+$string['char_b'] = 'B';
+$string['char_c'] = 'C';
+
+//errors
+$string['empty_field'] = 'Empty field';
+$string['wrong_dates'] = 'Wrong dates';
