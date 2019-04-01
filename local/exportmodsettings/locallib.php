3b2706437081add447f4b763cb3e72e5c6b15537
oleg <oleg@devlion.co>
2019-04-01 12:07:34 +0300

    Update result value (round)

--- local/exportmodsettings/locallib.php
+++ local/exportmodsettings/locallib.php
@@ -263,9 +263,9 @@ function local_exportmodsettings_generate_output_csv($output, $postdata = array(
 
             $data[$num]['MOODLE_ID'] = $item->moodle_id;
             $data[$num]['ASSIGN_NAME'] = htmlspecialchars_decode(trim(str_replace(',', ' ', $item->assign_name)));
-            $data[$num]['WEIGHT'] = round($item->weight, 5);
+            $data[$num]['WEIGHT'] = round($item->weight);
             $data[$num]['OBLIGATORY'] = $item->obligatory;
-            $data[$num]['PASS_GRADE'] = round($item->pass_grade, 5);
+            $data[$num]['PASS_GRADE'] = round($item->pass_grade);
 
             $data[$num]['ASSIGN_REQ'] = ($item->count_children)?$item->count_children:'';
             $data[$num]['ASSIGN_FOR_AVG'] = ($item->count_children)?$item->count_children:'';
